0ba4199c6013f3afb934db8a60d50d87eab679d7
Luka Ivelić <ivelicluka@gmail.com>
2026-04-14 02:06:17 +0200

    fix: fixes

--- mycelium-console/src/auth.ts
+++ mycelium-console/src/auth.ts
@@ -3,7 +3,6 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   basePath: "/api/auth",
-  redirectProxyUrl: "http://localhost:3001/console/api/auth",
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
